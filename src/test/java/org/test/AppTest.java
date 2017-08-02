29a4f1e2ac477406dc46518e3f6c50b065e11286
vera <vera.tsvetkova@itsoft.bg>
2017-08-02 11:24:07 +0300

    second commit comentar

---
+++ src/test/java/org/test/AppTest.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.junit.Test;
+
+/**
+ * Created by zhivkokasabov on 7/24/2017.
+ */
+public class AppTest {
+
+    @Test
+    public  void main()
+
+    {
+        System.out.println( "Hello4 World!" );
+    }
+
+}
---
+++ src/test/java/org/test/TestJava.java
@@ -0,0 +1,14 @@
+package org.test;
+
+/**
+ * Created by zhivkokasabov on 7/24/2017.
+ */
+public class TestJava {
+
+
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Hello111111 World!22222" );
+    }
+}
